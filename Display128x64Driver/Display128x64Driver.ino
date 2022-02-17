fe234febf258b48f31aa49762a06b63e5fd429d7
bradleyshelley99 <shelleyb@ufl.edu>
2022-02-17 13:00:15 -0500

    Added some display drivers for 128x64 OLED
    Display driver in testing has some errors in the display calling simple fix.

---
+++ Display128x64Driver/Display128x64Driver.ino
@@ -0,0 +1,35 @@
+#include <Wire.h>               // SCL pin 19, SDA pin 18
+#include <Adafruit_GFX.h>      
+#include <Adafruit_SSD1306.h>   
+
+Adafruit_SSD1306 display(128, 64, &Wire, -1, 1000000);  // 1MHz I2C clock
+
+void setup()
+{
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  delay(100);
+  display.clearDisplay();
+  display.display();
+}
+
+void loop()
+{
+  display.clearDisplay(); // clear buffer
+  for (int r = 0; r < 9; r++)
+  {
+    for (int y = 0; y < 8; y++) // fill buffer completely withs chars
+    {
+      display.setTextSize(1);
+      display.setTextColor(1);
+      display.setCursor(0, (y * 8));
+      display.print("012345678901234567890");
+    }
+    display.setTextSize(1); // erase one line at the time
+    display.setTextColor(0);
+    display.setCursor(0, (r * 8));
+    display.print("012345678901234567890");
+
+    display.display();
+    delay(200);
+  }
+}
--- VNH5019Testing/VNH5019Testing.ino
+++ VNH5019Testing/VNH5019Testing.ino
@@ -10,9 +10,12 @@
  * Edited  14 Feb. 2022 Bradley Shelley
  */
 
+// The Drivers for Display need to be fixed. I have miss-copied them but its simple fix.
 // Provides same functionality as the VNH5019 just has some different current/voltage specs
 #include <VNH3SP30.h>
-
+#include <Wire.h>               // SCL pin 19, SDA pin 18
+#include <Adafruit_GFX.h>      
+#include <Adafruit_SSD1306.h>   
 
 VNH3SP30 Motor1;    // define control object for 1 motor
 
@@ -23,39 +26,110 @@ VNH3SP30 Motor1;    // define control object for 1 motor
 #define M1_DIAG 3   // diagnose pins (combined DIAGA/ENA and DIAGB/ENB)
 #define M1_CS 4    // current sense pin // Not Really Needed for our use case
 
+Adafruit_SSD1306 display(128, 64, &Wire, -1, 1000000);  // 1MHz I2C clock
+
 void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  delay(100);
+  display.clearDisplay();
+  display.display();
   Motor1.begin(M1_PWM, M1_INA, M1_INB, M1_DIAG, M1_CS);    // Motor 1 object connected through specified pins 
-  Serial.begin(115200);   
 }
 
 void loop() {
-  Serial.println("3/4 speed forward");
+  display.setTextSize(1);
+  display.setTextColor(1);
+  display.setCursor(0,0);
+  display.print("3/4 speed forward");
+  display.display();
+
   Motor1.setSpeed(300); // motor 3/4-speed "forward"
   delay(2000); // wait for 2 seconds
+  display.setCursor(0,8);
+  display.print("Current="); 
+  display.print(Motor1.motorCurrent());
+  display.display();
   Serial.print("Current="); Serial.println(Motor1.motorCurrent());
 
+
+  delay(500);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(1);
+  display.setCursor(0,0);
+  display.print("Motor stop (coast)");
   Serial.println("Motor stop (coast)");
   Motor1.setSpeed(0); // motor stop (coasting)
   delay(2000); // wait for 2 seconds
+  display.setCursor(0,8);
+  display.print("Current="); 
+  display.print(Motor1.motorCurrent());
+  delay(200);
   Serial.print("Current at stop="); Serial.println(Motor1.motorCurrent());
+  display.display();
+  delay(200);
 
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(1);
+  display.setCursor(0,0);
+  display.print("Half speed backward");
   Serial.println("Half speed backward");
   Motor1.setSpeed(-200); // motor half-speed "backward"
+  display.display();
+
   delay(2000); // wait for 2 seconds
- 
+  
+  display.setCursor(0,8);
+  display.print("Current="); 
+  display.print(Motor1.motorCurrent());
+  delay(200);
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(1);
+  display.setCursor(0,0);
+  display.print("Motor stop (coast)");
   Serial.println("Motor stop (coast)");
   Motor1.setSpeed(0); // motor stop 
   delay(2000); // wait for 2 seconds
+  display.setCursor(0,8);
+  display.print("Current="); 
+  display.print(Motor1.motorCurrent());
+  delay(200);
 
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(1);
+  display.setCursor(0,0);
+  display.print("Full speed backward");
   Serial.println("Full speed backward");
   Motor1.setSpeed(-400); // motor full-speed "backward"
   delay(2000); // wait for 2 seconds
+  display.setCursor(0,8);
+  display.print("Current="); 
+  display.print(Motor1.motorCurrent());
   Serial.print("Current="); Serial.println(Motor1.motorCurrent());
+  delay(200);
 
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(1);
+  display.setCursor(0,0);
+  display.print("Break at 3/4 power");
   Serial.println("Break at 3/4 power");
   Motor1.brake(300); // motor brake at 3/4 power
   delay(10);
+  display.setCursor(0,8);
+  display.print("Current during brake="); 
+  display.print(Motor1.motorCurrent());
   Serial.print("Current during brake="); Serial.println(Motor1.motorCurrent());
   delay(4000); // wait for 4 seconds
+  display.setCursor(0,8);
+  display.print("Current after brake="); 
+  display.print(Motor1.motorCurrent());
   Serial.print("Current after brake="); Serial.println(Motor1.motorCurrent());
+  delay(200);
+
 }
